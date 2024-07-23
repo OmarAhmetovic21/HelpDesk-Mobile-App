ad8f3f8161cba960aceaab056073b157c17e8876
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2024-07-23 12:09:33 +0200

    Login page created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,118 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
+import { Platform, View, Text, TextInput, Button, StyleSheet, Image,Pressable } from 'react-native';
+import React, { useState } from 'react';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View>
+      <Image source={require('@/assets/images/logo_samo.png')} style={styles.logo} />
+      </View>
+      <View style={styles.titleView}>
+      <Text style={styles.title}>Sarajevogas Helpdesk</Text>
+      </View>
+      <Text style={styles.label}>Login</Text>
+      <Text style={styles.labelCaption}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="ime.prezime@sarajevogas.ba"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+      <Text style={styles.labelCaption}>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Unesi svoj password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {/*<Button title="Login" onPress={handleLogin}/>*/}
+      <Pressable style={({ pressed }) => [
+          {
+            backgroundColor: pressed
+              ? '#004494'
+              : '#0056b3'
+          },
+          styles.button
+        ]} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    padding: 16,
+    backgroundColor: '#fff',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  logo: {
+    width: 100,
+    height: 100,
+    //marginBottom: 100
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+    //justifyContent: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign:'center',
+    marginTop:'2.5%'
   },
+  label: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    marginBottom: 30,
+    color: '#224798'
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  titleView: {
+    backgroundColor:'#007F37',
+    width: '100%', 
+    height:'7%',
+    marginBottom: 60
+  },
+    button: {
+      width: '100%',
+      height: 40,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 4,
+      marginBottom: 300,
+      marginTop: 20
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    labelCaption:{
+      fontSize: 17,
+      marginBottom: 10,
+      color: '#224798'
+    }
 });
