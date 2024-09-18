108841397205516909c8178031d310a7c973c039
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2024-09-18 12:19:21 +0200

    Added padding between close and send buttons

--- screens/Report-Issue-Screen/Report-Issue-Screen.js
+++ screens/Report-Issue-Screen/Report-Issue-Screen.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import {View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
-const ReportIssueScreen  = ({ navigation }) =>{
+const ReportIssueScreen = ({ navigation }) => {
   const [selectedSektor, setSelectedSektor] = useState("");
+
   return (
     <View style={styles.container}>
 
       <View style={styles.loginHeader}>
         <Text style={styles.loginTitle}>Prijavite Smetnju:</Text>
       </View>
+
       <View style={styles.formContainer}>
-      <TextInput
+        <TextInput
           placeholder="Ime i Prezime"
           style={styles.inputName}
           autoCapitalize="none"
@@ -25,24 +27,39 @@ const ReportIssueScreen  = ({ navigation }) =>{
         <Text style={styles.label}>Poruka:</Text>
         <TextInput
           style={styles.inputMessage}
+          multiline={true}
+          numberOfLines={4} // Minimalni broj linija
+          textAlignVertical="top"
         />
-      <Picker
-        selectedValue={selectedSektor}
-        onValueChange={(itemValue) => setSelectedSektor(itemValue)}
-        style={styles.picker}
-      >
-        <Picker.Item label="Izaberite Sektor" value="" />
-        <Picker.Item label="Sektor 1" value="sektor1" />
-        <Picker.Item label="Sektor 2" value="sektor2" />
-        <Picker.Item label="Sektor 3" value="sektor3" />
-      </Picker>
-        <Button title="Spasi" color="#007F37" onPress={() => navigation.navigate('AdminDashboard')} style={styles.buttonSave} />
-        <Button title="Zatvori" color="#ff0808" onPress={() => navigation.navigate('AdminDashboard')} style={styles.buttonClose} />
+        <Picker
+          selectedValue={selectedSektor}
+          onValueChange={(itemValue) => setSelectedSektor(itemValue)}
+          style={styles.picker}
+        >
+          <Picker.Item label="Izaberite Sektor" value="" />
+          <Picker.Item label="Sektor 1" value="sektor1" />
+          <Picker.Item label="Sektor 2" value="sektor2" />
+          <Picker.Item label="Sektor 3" value="sektor3" />
+        </Picker>
+
+        {/* Razmak između dugmića postignut korištenjem margine ispod svakog dugmeta */}
+        <View style={styles.buttonWrapper}>
+          <Button
+            title="Pošalji"
+            color="#007F37"
+            onPress={() => navigation.navigate('AdminDashboard')}
+          />
+        </View>
+
+        <View style={styles.buttonWrapper}>
+          <Button
+            title="Zatvori"
+            color="#ff0808"
+            onPress={() => navigation.navigate('AdminDashboard')}
+          />
+        </View>
       </View>
 
-      
-      
-      
     </View>
   );
 };
@@ -53,13 +70,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 20,
   },
-  title: {
-    fontSize: 21,
-    fontWeight: 'bold',
-    color: 'white',
-  },
   loginHeader: {
-    height: 50, // Adjust the flex value as needed
+    height: 50,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
@@ -72,13 +84,12 @@ const styles = StyleSheet.create({
     color: '#007F37',
   },
   formContainer: {
-    height: '30%', // Adjust the flex value as needed
     width: '100%',
   },
   label: {
     fontSize: 18,
     marginBottom: 5,
-    color:'#AFAFAF',
+    color: '#AFAFAF',
   },
   inputEmail: {
     width: '100%',
@@ -86,49 +97,34 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,
     borderColor: '#AFAFAF',
-    borderTopWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
     borderBottomWidth: 2,
   },
   inputName: {
     width: '100%',
     height: 40,
-    borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
-    borderRadius: 0,
     borderColor: '#007F37',
-    borderTopWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
     borderBottomWidth: 2,
   },
   inputMessage: {
     width: '100%',
-    height: 80,
-    borderColor: 'AFAFAF',
+    height: 90,
+    borderColor: '#AFAFAF',
     marginBottom: 10,
     borderRadius: 9,
-    borderTopWidth: 2,
-    borderLeftWidth: 2,
-    borderRightWidth: 2,
-    borderBottomWidth: 2,
+    borderWidth: 2,
+    padding: 5,
   },
-  buttonClose: {
-    color: 'white',
-    
-  },
-  buttonSave: {
-    color: 'white',
-    marginBottom: 10,
+  buttonWrapper: {
+    marginBottom: 10, // Razmak između dugmadi
   },
   picker: {
     backgroundColor: '#D9D9D9',
     borderRadius: 3,
     color: '#0056b3',
+    marginBottom: 10,
   },
-
 });
 
 export default ReportIssueScreen;
